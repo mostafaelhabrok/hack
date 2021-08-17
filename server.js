34dd6c6dd5ec1e946f42f49af92aa043983782f7
mostafaelhabrok <mostafa.elhabrok@gmail.com>
2021-08-17 16:42:16 +0200

    1

---
+++ server.js
@@ -0,0 +1,45 @@
+// Setup empty JS object to act as endpoint for all routes
+lol = {};
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+
+//const port = 8000;
+const port = process.env.PORT || 80
+
+const server = app.listen(port, listening);
+function listening(){
+    console.log("server running"); 
+    console.log('running on localhost: '+port);
+}
+
+
+  //Get Data
+app.post('/test', function (request, response) {
+    console.log(request.body);
+    lol = request.body;
+    response.send({"lol":request.body})
+  });
+
+    //Get Data
+app.get('/test2', function (request, response) {
+    //console.log(lol);
+    
+    response.send({...lol})
+  });
